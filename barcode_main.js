4a08111d040a00ef54fa368a72e20f7215dfad66
skilldrick <skilldrick@gmail.com>
2011-02-23 14:30:00 +0000

    Drawing bars at correct width.

--- barcode_main.js
+++ barcode_main.js
@@ -18,18 +18,75 @@ function showDialog() {
   }
 }
 
+function DrawLineMaker() {
+  var doc = getCurrentOrNewDocument();
+  var page = app.activeWindow.activePage;
+  return function (x1, y1, x2, y2) {
+    var pathPoints = page.graphicLines.add().paths[0].pathPoints;
+    pathPoints[0].anchor = [x1, y1];
+    pathPoints[1].anchor = [x2, y2];
+  }
+}
+
+function DrawBoxMaker() {
+  var doc = getCurrentOrNewDocument();
+  var page = app.activeWindow.activePage;
+  return function (x, y, width, height) {
+    var rect = page.rectangles.add();
+    rect.strokeWeight = 0;
+    rect.fillColor = "Black";
+    rect.geometricBounds = [y, x, y + height, x + width];
+  }
+}
+
+function getCurrentOrNewDocument() {
+  var doc = app.documents[0];
+  if (!doc.isValid) {
+    doc = app.documents.add();
+  }
+  return doc;
+}
+
 function drawBarcode(barWidths) {
-  var height = 20;
+  var height = 70;
+  var guardHeight = 75;
+  var reduce = 0.3;
   var pattern = null;
   var widths = null;
+  var width = 0;
+  var drawLine = DrawLineMaker();
+  var drawBox = DrawBoxMaker();
+  var hpos = 0;
+
+  drawBox(hpos, 0, 1-reduce, guardHeight);
+  hpos += 2;
+  drawBox(hpos, 0, 1-reduce, guardHeight);
+  hpos += 1;
+
   for (var i = 0; i < barWidths.length; i++) {
     pattern = barWidths[i][0];
     widths = barWidths[i][1];
+    for (var j = 0; j < 4; j++) {
+      width = widths[j];
+      if (pattern[j] === 1) {
+        drawBox(hpos, 0, width-reduce, height-reduce);
+      }
+      hpos += width;
+    }
 
-    //pattern is e.g. [0, 1, 0, 1] - i.e. white, black, white, black
-    //widths is e.g. [1, 2, 1, 3] - i.e. 1 white, 2 black, 1 white, 3 black
+    if (i == 5) {
+      hpos += 1;
+      drawBox(hpos, 0, 1-reduce, guardHeight);
+      hpos += 2;
+      drawBox(hpos, 0, 1-reduce, guardHeight);
+      hpos += 2;
+    }
   }
 
+  drawBox(hpos, 0, 1-reduce, guardHeight);
+  hpos += 2;
+  drawBox(hpos, 0, 1-reduce, guardHeight);
+  hpos += 2;
 }
 
 var isbn = showDialog();
