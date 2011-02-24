7ff891e9da4f9d59ee6c893dc75008cb42ecedb6
skilldrick <skilldrick@gmail.com>
2011-02-24 14:28:18 +0000

    Refactored barcode drawing funcs into BarcodeDrawer object.

--- barcode_main.js
+++ barcode_main.js
@@ -18,81 +18,126 @@ function showDialog() {
   }
 }
 
-function DrawLineMaker() {
-  var doc = getCurrentOrNewDocument();
-  var page = app.activeWindow.activePage;
-  return function (x1, y1, x2, y2) {
+var BarcodeDrawer = (function () {
+  var doc;
+  var page;
+  var scale;
+  var height;
+  var guardHeight;
+  var reduce;
+  var hpos;
+  var vOffset;
+
+  function drawLine(x1, y1, x2, y2) {
+    x1 *= scale;
+    y1 *= scale;
+    x2 *= scale;
+    y2 *= scale;
     var pathPoints = page.graphicLines.add().paths[0].pathPoints;
     pathPoints[0].anchor = [x1, y1];
     pathPoints[1].anchor = [x2, y2];
   }
-}
 
-function DrawBoxMaker() {
-  var doc = getCurrentOrNewDocument();
-  var page = app.activeWindow.activePage;
-  return function (x, y, width, height) {
+  function drawBox(x, y, width, height) {
+    x *= scale;
+    y *= scale;
+    width *= scale
+    height *= scale;
     var rect = page.rectangles.add();
     rect.strokeWeight = 0;
     rect.fillColor = "Black";
     rect.geometricBounds = [y, x, y + height, x + width];
   }
-}
 
-function getCurrentOrNewDocument() {
-  var doc = app.documents[0];
-  if (!doc.isValid) {
-    doc = app.documents.add();
+  function getCurrentOrNewDocument() {
+    var doc = app.documents[0];
+    if (!doc.isValid) {
+      doc = app.documents.add();
+    }
+    return doc;
   }
-  return doc;
-}
 
-function drawBarcode(barWidths) {
-  var height = 70;
-  var guardHeight = 75;
-  var reduce = 0.3;
-  var pattern = null;
-  var widths = null;
-  var width = 0;
-  var drawLine = DrawLineMaker();
-  var drawBox = DrawBoxMaker();
-  var hpos = 0;
+  function drawBar(width) {
+    drawBox(hpos, vOffset, width - reduce, height);
+  }
 
-  drawBox(hpos, 0, 1-reduce, guardHeight);
-  hpos += 2;
-  drawBox(hpos, 0, 1-reduce, guardHeight);
-  hpos += 1;
+  function drawGuard() {
+    drawBox(hpos, vOffset, 1 - reduce, guardHeight);
+  }
 
-  for (var i = 0; i < barWidths.length; i++) {
-    pattern = barWidths[i][0];
-    widths = barWidths[i][1];
-    for (var j = 0; j < 4; j++) {
-      width = widths[j];
-      if (pattern[j] === 1) {
-        drawBox(hpos, 0, width-reduce, height-reduce);
+  function startGuards() {
+    drawGuard();
+    hpos += 2;
+    drawGuard();
+    hpos += 1;
+  }
+
+  function midGuards() {
+    hpos += 1;
+    drawGuard();
+    hpos += 2;
+    drawGuard();
+    hpos += 2;
+  }
+
+  function endGuards() {
+    drawGuard();
+    hpos += 2;
+    drawGuard();
+    hpos += 2;
+  }
+
+  function drawMain(barWidths) {
+    var pattern = null;
+    var widths = null;
+    var width = null;
+
+    for (var i = 0; i < barWidths.length; i++) {
+      pattern = barWidths[i][0];
+      widths = barWidths[i][1];
+      for (var j = 0; j < 4; j++) {
+        width = widths[j];
+        if (pattern[j] === 1) {
+          drawBar(width);
+        }
+        hpos += width;
       }
-      hpos += width;
-    }
-
-    if (i == 5) {
-      hpos += 1;
-      drawBox(hpos, 0, 1-reduce, guardHeight);
-      hpos += 2;
-      drawBox(hpos, 0, 1-reduce, guardHeight);
-      hpos += 2;
+      if (i == 5) {
+        midGuards();
+      }
     }
   }
 
-  drawBox(hpos, 0, 1-reduce, guardHeight);
-  hpos += 2;
-  drawBox(hpos, 0, 1-reduce, guardHeight);
-  hpos += 2;
-}
+  function init() {
+    scale = 0.3;
+    height = 70;
+    guardHeight = 75;
+    reduce = 0.3;
+    hpos = 0;
+    vOffset = 0;
+    doc = getCurrentOrNewDocument();
+    page = app.activeWindow.activePage;
+    var viewPrefs = doc.viewPreferences;
+    viewPrefs.horizontalMeasurementUnits = MeasurementUnits.millimeters;
+    viewPrefs.verticalMeasurementUnits = MeasurementUnits.millimeters;
+  }
+
+  function drawBarcode(barWidths) {
+    init();
+    startGuards();
+    drawMain(barWidths);
+    endGuards();
+  }
+
+  return {
+    drawBarcode: drawBarcode
+  }
+})();
+
 
 var isbn = showDialog();
 if (isbn) {
   var barWidths = Barcode().init(isbn).getNormalisedWidths();
-
-  drawBarcode(barWidths);
+  BarcodeDrawer.drawBarcode(barWidths);
 }
 
